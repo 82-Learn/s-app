d3daa81f133e0d09e1e3744b2e77ba432d418dc8
ss <surinder@82learn.com>
2022-06-23 03:17:25 +1000

    resize changes 2

--- src/globals/global.styles.tsx
+++ src/globals/global.styles.tsx
@@ -1,14 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
-
 export const GlobalStyle = createGlobalStyle`
     body {
 
   background-color: rgb(26, 26, 26);
- 
+/* Portrait and Landscape */
+@media only screen 
+  and (min-device-width: 414px) 
+  and (max-device-width: 736px) 
+  and (-webkit-min-device-pixel-ratio: 3) { 
 
+}
     }
   button {
     cursor: pointer;
   }
+
 `;
