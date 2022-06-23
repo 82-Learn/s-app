85bab9445728c2fe9c47cc4dcd0d89a03734d014
ss <surinder@82learn.com>
2022-06-24 02:25:02 +1000

    new updates

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import Amplify from "aws-amplify";
-import config from "./aws-exports";
-Amplify.configure(config);
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 
