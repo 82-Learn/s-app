27b2a756c44a0665107f31f15afaabcb091d7246
ss <surinder@82learn.com>
2022-07-14 05:12:19 +1000

    amplify remove folder

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-//import Amplify from "aws-amplify";
-//import awsconfig from "./aws-exports";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
 
-//Amplify.configure(awsconfig);
+Amplify.configure(awsconfig);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
