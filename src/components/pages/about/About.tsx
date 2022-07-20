752b49777bd1d091ff1ca32c3a7cfb7756d7075a
ss <surinder@82learn.com>
2022-07-21 04:56:30 +1000

    updates

--- src/components/pages/about/About.tsx
+++ src/components/pages/about/About.tsx
@@ -1,26 +1,24 @@
 import * as React from "react";
 import styled from "styled-components";
 
-
 const BaseComponent = styled.div`
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(20px + 3vmin);
-    color: white;
-    background-color: black;
-
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(20px + 3vmin);
+  color: white;
+  background-color: black;
 `;
 
-
 const About = () => {
-    return (
-        <BaseComponent>
-            <p>ABOUT PAGE</p>
-        </BaseComponent>
-    );
+  return (
+    <BaseComponent>
+      <p>ABOUT PAGE</p>
+      <h1>Testing ths page</h1>
+    </BaseComponent>
+  );
 };
 
 export default About;
